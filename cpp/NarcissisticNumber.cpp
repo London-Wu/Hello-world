15d36d6a665cdc9c630cd5c3c96a4909f676071e
London-Wu <www.787330410@qq.com>
2018-11-28 22:26:31 +0800

    11-28 NarcissisticNumber.cpp

--- cpp/NarcissisticNumber.cpp
+++ cpp/NarcissisticNumber.cpp
@@ -1,1 +1,16 @@
- 	
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x,y,z;
+	for(int num=100;num<1000;num++){
+		x=num%10;
+		y=num/10%10;
+		z=num/100;
+		if (x*x*x+y*y*y+z*z*z==num){
+			cout << num << " ";
+		}
+	}
+
+	return 0;
+}
