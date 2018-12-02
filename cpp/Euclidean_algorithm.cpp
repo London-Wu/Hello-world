db82a3c309e85d0d407693e6a9e666f6dae63ddf
London-Wu <www.787330410@qq.com>
2018-12-02 23:02:36 +0800

    change the Euclidean_algorithm.cpp

--- cpp/Euclidean_algorithm.cpp
+++ cpp/Euclidean_algorithm.cpp
@@ -1,22 +1,27 @@
-//辗转相除法(欧几里得算法)求最大公因数
+//辗转相除法(欧几里得算法)求最大公因数及最小公倍数
 #include <iostream>
 using namespace std;
 
 int main(){
-	int num1;
-	int num2;
+	int num1,num2,a,b,temp;
 	cout << "input two numbers:";
 	cin >> num1 >> num2;
+	a=num1;
+	b=num2;
+	if(a<b){
+		temp=a;
+		a=b;
+		b=temp;
+	}
 
-	int temp;
-
-	while(temp!=0){
-		temp=num1%num2;
-		num1=num2;
-		num2=temp;
+	while(b!=0){
+		temp=a%b;
+		a=b;
+		b=temp;
 		
 	}
-	cout << "两个数的最大公约数为:" << num1 << endl;
+	cout << num1 << "和" << num2 << "的最大公约数为:" << a << endl;
+	cout << num1 << "和" << num2 <<  "的最小公倍数为:" << num1 * num2 / a << endl;
 
 	return 0;
 }
